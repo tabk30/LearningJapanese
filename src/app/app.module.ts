0d6a2a4ab77470b45f6804ae1c402928292f724b
AnhPT <anhpt@ows.vn>
2017-07-07 09:11:51 +0700

    Add auto update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -11,23 +12,30 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '0581f9bf'
+  }
+};
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     ContactPage,
+    AboutPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
     ContactPage,
+    AboutPage,
     HomePage,
     TabsPage
   ],
